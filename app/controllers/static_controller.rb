77d1ebf44f831777cad6520de822a2e81ae3a161
Michael LaCroix <michael@lacroixdesign.net>
2013-05-15 18:50:19 -0500

    refactored & restyled intro

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,19 @@
 class StaticController < ApplicationController
 
+  # GET /
+  def index
+    @tees = [
+      { image: "drum-tee.png",    alt: "Beat Cancer Rock Tee",       slug: "1286516-beat-cancer-rock-t-shirt" },
+      { image: "mic-tee.png",     alt: "Music Speaks Tee",           slug: "1435182-music-speaks-t-shirt" },
+      { image: "fighter-tee.png", alt: "The Spirt of a Fighter Tee", slug: "1435354-the-spirt-of-a-fighter-t-shirt" }
+    ]
+  end
+
   # POST /newsletter
   def newsletter
     @subscription = NewsletterSubscribe.new params[:model]
     @subscription.submit
     render json: @subscription.result, status: @subscription.status
   end
-  
+
 end
