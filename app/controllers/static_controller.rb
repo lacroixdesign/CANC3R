3728f02725f07b920def2848e0d3a57ee3d2de1c
James LaCroix <james@lacroixdesign.net>
2015-05-20 19:41:58 -0500

    Change tees

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,9 +4,9 @@ class StaticController < ApplicationController
   def index
     # Tees used in the #intro
     @tees = [
-      { image: "mens.png",    alt: "Men's Purple Fighter Tee",       slug: "copy-of-fighter-tee" },
-      { image: "ladies.png",     alt: "Ladies's Purple Fighter Tee",           slug: "female-purple-fighter-tee" },
-      { image: "childrens.png", alt: "Childrens's Purple Fighter Tee", slug: "youth-purple-fighter-tee" }
+      { image: "drum-tee.png",    alt: "Beat Cancer Rock Tee",       slug: "store/beat-cancer-1" },
+      { image: "fighter-tee.png", alt: "The Spirt of a Fighter Tee", slug: "store/fighter-tee" },
+      { image: "mens.png",    alt: "Men's Purple Fighter Tee",       slug: "?category=CANC3R" }
     ]
   end
 
